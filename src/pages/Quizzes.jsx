566ddf2e1a7a070955b060458320e4f5bdf508e3
Ernazar <kuanishbayevyernazar@gmail.com>
2024-07-31 15:11:22 +0500

    New file was added.

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -5,10 +5,18 @@ const Quizzes = () => {
     <div className='h-screen bg-gradient-to-r from-sky-500 to-indigo-500 flex justify-center items-center'>
       <div className="w-1/2">
         <p className="text-white text-3xl text-center font-bold mb-10">Testler</p>
-        <ul>
+        <ul className='flex flex-col gap-4'>
           <a href='/test-files/1-test.exe' download>
             <li className="bg-white p-4 rounded-md cursor-pointer hover:bg-orange-200 shadow active:scale-95 transition-all active:bg-white flex justify-between items-center gap-4">
               <span className="text-indigo-600 text-lg font-semibold">1-test</span>
+              <div className='h-10'>
+                <img src={brainIcon} alt="Brain icon" className='h-full' />
+              </div>
+            </li>
+          </a>
+          <a href='/test-files/2-test.exe' download>
+            <li className="bg-white p-4 rounded-md cursor-pointer hover:bg-orange-200 shadow active:scale-95 transition-all active:bg-white flex justify-between items-center gap-4">
+              <span className="text-indigo-600 text-lg font-semibold">2-test</span>
               <div className='h-10'>
                 <img src={brainIcon} alt="Brain icon" className='h-full' />
               </div>
